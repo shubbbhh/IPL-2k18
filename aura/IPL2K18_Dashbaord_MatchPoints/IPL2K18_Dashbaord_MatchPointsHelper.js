aa75447a94fad221d1ddd1b9574d9206bd99e35c
aman_porwal <aman_porwal@HJD23552.persistent.co.in>
2017-12-07 19:29:47 +0530

    Added by Aman

---
+++ aura/IPL2K18_Dashbaord_MatchPoints/IPL2K18_Dashbaord_MatchPointsHelper.js
@@ -0,0 +1,45 @@
+({
+    setupRadarChart : function(component,message) {
+        if(message.contains('aman'))
+        var action = component.get("c.getDashbaordMatchDetails");
+        if(message == 'porwal')
+        var action = component.get("c.getDashbaordMatchDetails");
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            alert('state'+state);            
+            var jsonRetVal = JSON.parse(response.getReturnValue()); 
+            alert(jsonRetVal.teamNameStr);
+            var el = component.find('barChart').getElement();
+            var ctx = el.getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: jsonRetVal.teamNameStr,
+                    datasets: [
+                        {
+                            label: "Teams",
+                            backgroundColor: ["#3e40cd", "#3e40cd","#3cba3f","#e8c3c9","#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                            data: jsonRetVal.teamPointsInt
+                        }
+                    ]
+                },
+                options: {
+                    legend: { display: true },
+                    title: {
+                        display: true,
+                        text: 'Cricket Actual Team Table'
+                    },
+                    responsive:true,
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            
+                        }
+                    }]
+                }
+            });
+        })
+        $A.enqueueAction(action);   
+        
+    }
+})
